24d832ccd7485082b451a862888002a8c86fd3fd
shohan98 <dshohan112@gmail.com>
2019-04-04 18:08:36 +0600

    my comment about the changes

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="AlarmTimeClaculate",
+    version="0.0.1",
+    author="Md. Shohanur Rahman",
+    author_email="dshohan112@gmail.com",
+    description="Detect alarm time from natural sentence",
+#    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
