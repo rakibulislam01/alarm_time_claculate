2ee611e30b5a6ef31334ad79bc3b9d581a4b69e9
shohan98 <dshohan112@gmail.com>
2019-04-18 13:50:16 +0600

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-name = "AlarmTimeClaculate"
+name = "AlarmTime"
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import setuptools
 
 setuptools.setup(
-    name="AlarmTimeClaculate",
-    version="0.0.1",
+    name="AlarmTime",
+    version="0.1",
     author="Md. Shohanur Rahman",
     author_email="dshohan112@gmail.com",
     url = 'https://github.com/shohan98/alarm_time_claculate',
     description="Detect alarm time from natural sentence and calculate time difference from current time to target time.",
-#    long_description=long_description,
+    long_description='For calculating time difference from current time to Target time and can detect a time from our natural language',
+    long_description_content_type="text/markdown",
+    license = 'MIT',
+    install_requires = ['datetime','python-dateutil','regex'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
